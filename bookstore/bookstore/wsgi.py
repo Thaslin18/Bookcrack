f988641bc98b2c67bfd6151a7a9cd64f66caaadc
Thaslin <thaslin18parveen@gmail.com>
2026-08-27 20:31:17 +0530

    Prepare Django project for vercel  deployment

--- bookstore/bookstore/wsgi.py
+++ bookstore/bookstore/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookstore.bookstore.settings')
+# Add the inner bookstore directory to the system path
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookstore.settings')
 
 application = get_wsgi_application()
